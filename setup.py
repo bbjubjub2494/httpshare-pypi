020b478e5e93e209045f5bb6ac3716ef83526037
Louis Bettens <louis@bettens.info>
2018-04-11 22:50:50 +0200

    release 1.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 
 setup(
     name='httpshare',
-    version='1.0.4',
+    version='1.0.5',
     description='Q&D file transfer utility using an ephemeral HTTP service',
 
     long_description=io.open('README.rst', encoding='utf-8').read(),
@@ -46,6 +46,7 @@ setup(
 
     install_requires=[
         'bottle==0.12.13',
+        'colorama==0.3.9',
         'docopt==0.6.2',
         'qrcode==6.0',
     ],
