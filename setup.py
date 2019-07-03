0b4094d84a832e8705750d41dde1098aa48362ff
Louis Bettens <louis@bettens.info>
2019-07-03 16:38:47 +0200

    loosen PyPI dependencies

--- setup.py
+++ setup.py
@@ -45,10 +45,10 @@ setup(
     zip_safe=True,  # by design
 
     install_requires=[
-        'bottle==0.12.16',
-        'colorama==0.4.1',
-        'docopt==0.6.2',
-        'qrcode==6.0',
+        'bottle ~= 0.12.16',
+        'colorama ~= 0.4.1',
+        'docopt ~= 0.6.2',
+        'qrcode ~= 6.0',
     ],
 
     entry_points={
